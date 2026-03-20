ccf763ed3344d9406b4b517ee9c1ebc3da45287b
scivision <scivision@users.noreply.github.com>
2026-03-20 00:40:35 -0400

    fdisc signature match

--- src/fortran_source2d/fdisc.f
+++ src/fortran_source2d/fdisc.f
@@ -1,7 +1,8 @@
-      double precision function fdisc(blockno,x,y)
+      double precision function fdisc(blockno,x,y,z)
       implicit none
 
-      double precision x,y
+      double precision :: x,y
+      double precision, optional :: z
       integer blockno
 
       fdisc = 0
